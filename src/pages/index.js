717ff00201395765baf257c5557db63f2e24b39f
Shelby Stanton <shelbylouise72@gmail.com>
2019-10-05 20:41:19 +0100

    fix: add padding to container

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
   <>
     <SEO title="Home" />
     <div className="container mx-auto">
-      <div className="py-24">
+      <div className="py-24 px-2">
         <h1 className="text-5xl font-semibold">Hey! I'm Shelby.</h1>
         <p className="text-2xl opacity-50">A developer who loves JavaScript.</p>
       </div>
